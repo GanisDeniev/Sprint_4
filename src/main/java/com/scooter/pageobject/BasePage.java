2fe957d63caba09ea17d3b98e02dffe8e669a677
Denis Ganiev <densw-gg@yandex.ru>
2024-02-07 14:24:22 +0300

    Uploading project

--- src/main/java/com/scooter/pageobject/BasePage.java
+++ src/main/java/com/scooter/pageobject/BasePage.java
@@ -1,8 +1,6 @@
 package com.scooter.pageobject;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
     public static final String URL = "https://qa-scooter.praktikum-services.ru/";
